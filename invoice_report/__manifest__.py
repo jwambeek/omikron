6f609902cc325cb130c14685c5befc4b88323f2c
nabeelwaoconnect  <rananabeel052@gmail.com>
2023-02-07 07:07:47 +0500

    [ADD] add tax invoice

---
+++ invoice_report/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Invoice Reports",
+    "summary": "Invoice Reports",
+    "version": "16.0.0.0",
+    "maintainers": ["WAO"],
+    "category": "Accounting",
+    "website": "http://www.waoconnect.com",
+    "author": "WAO",
+    "depends": ['web','account'],
+    "data": [
+        "reports/tax_invoice.xml",
+        "reports/reports.xml",
+    ],
+    "application": False,
+    'auto_install': False,
+    "installable": True
+}
---
+++ invoice_report/models/__init__.py
@@ -0,0 +1,2 @@
+
+from . import tax_invoice_model
