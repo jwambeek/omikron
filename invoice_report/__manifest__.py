1693c1c80882c9eac3444e3400f4178255647c1c
nabeelwaoconnect  <rananabeel052@gmail.com>
2023-02-08 11:59:05 +0500

    [FIX] fix preview page size

--- invoice_report/__manifest__.py
+++ invoice_report/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Invoice Reports",
     "summary": "Invoice Reports",
-    "version": "16.0.0.4",
+    "version": "16.0.0.5",
     "maintainers": ["WAO"],
     "category": "Accounting",
     "website": "http://www.waoconnect.com",
